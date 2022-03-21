ded5c7727c1770c52be5184a6d553ff166ea4014
mteiska <mika123450@hotmail.com>
2022-03-21 18:11:09 +0200

    search toimii

--- XML_RPC_project/server.py
+++ XML_RPC_project/server.py
@@ -26,7 +26,7 @@ with SimpleXMLRPCServer(('localhost', 9000),
             new_topic = ET.SubElement(root,"topic", name=topic)
          noteElement = ET.SubElement(new_topic, "note", name=title)
         
-         note1 = ET.SubElement(new_topic, "Text")
+         note1 = ET.SubElement(noteElement, "Text") #Since Text is under note and not topic we have to do this.
          note1.text = text
          timestampElement = ET.SubElement(noteElement, "timestamp")
          timestampElement.text = str(timestamp)
@@ -47,29 +47,36 @@ with SimpleXMLRPCServer(('localhost', 9000),
 
    # Lets register the note sending function
    server.register_function(send_note)
+   #Creatig function for searching with topic 
    def search_note(topic):
       try:
-        tree = ET.parse("notes.xml")
+        tree = ET.parse("db.xml")
         root = tree.getroot()
         notes_to_return = []
-        topicElement = root.find(topic)
-        if topicElement is None:
+        topicElement = root.find("topic[@name='{}']".format(topic))
+        if topicElement == None:
             print("No notes found")
-            return []
+            return [] # client waits for list to be returned this is good principle
         else:
-            for note in topicElement.findall("Text"):
-                notes_to_return.append(topic)
-                text = note.text
-                print("Topic on ", topic)
-                print("topicin teksti on", text)
-                notes_to_return.append(text)
-                timestamp = note.find("timestamp").text
-                notes_to_return.append(timestamp)
-            print("Found some note for ", topicElement)
-            return notes_to_return
-      except:
-        print("Something went wrong.")
-        return False
+            for topic_note in topicElement.findall("note"): #We want to search under note since Text is there and timestamp
+               notes_to_return.append(topic)
+               print(topic_note)
+               title = topic_note.get("name")
+               print("Title is ", title)
+               notes_to_return.append(title)
+               text = topic_note.find("Text").text
+               print(text)
+               notes_to_return.append(text)
+               print("Topic is ", topic)
+               print("topicin teksti on", text)
+         
+               timestamp = topic_note.find("timestamp").text
+               notes_to_return.append(timestamp)
+            print("Found some note for ", topic)
+            return notes_to_return #return list of all the notes under topic
+      except Exception as e:
+        print("Error with finding notes with topic",e)
+        return None #return none if error and print error message
 
 
    # Lets register the note searching function
@@ -77,9 +84,9 @@ with SimpleXMLRPCServer(('localhost', 9000),
 
 
       
-   print("funktiot valmiina käyttöön")
+   print("Functions are ready to use.")
    try:
-      print("Exit with Ctrl + c")
+      print("Exit with Ctrl + c or with 0")
       server.serve_forever()
    except KeyboardInterrupt:
       server.server_close()
